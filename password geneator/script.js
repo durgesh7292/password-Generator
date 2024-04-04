ecf18f162ba78ef40cfb1c698fcfa3306186917a
Durgesh kumar choudhary <137211425+durgesh7292@users.noreply.github.com>
2024-04-04 22:45:27 +0530

    Add files via upload

---
+++ password geneator/script.js
@@ -0,0 +1,51 @@
+let inputSlider = document.getElementById("inputSlider");
+let sliderValue = document.getElementById("sliderValue");
+let passBox = document.getElementById("passBox");
+let lowercase = document.getElementById("lowercase");
+let uppercase = document.getElementById("uppercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbols");
+let genBtn = document.getElementById("genBtn");
+let copyIcon = document.getElementById("copyIcon");
+
+sliderValue.textContent = inputSlider.value;
+inputSlider.addEventListener("input", () => {
+  sliderValue.textContent = inputSlider.value;
+});
+
+genBtn.addEventListener("click", () => {
+  passBox.value = generatePassword();
+});
+
+let lowechars = "abcdefghijklmnopqrstuvwxyz";
+let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let allNumbers = "0123456789";
+let allSymbols = "~!@#$%^&*";
+
+function generatePassword() {
+  let genPassword = "";
+  let allChars = "";
+
+  allChars += lowercase.checked ? lowechars : "";
+  allChars += uppercase.checked ? upperChars : "";
+  allChars += numbers.checked ? allNumbers : "";
+  allChars += symbols.checked ? allSymbols : "";
+
+  let i = 1;
+  while (i <= inputSlider.value) {
+    genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
+    i++;
+  }
+  return genPassword;
+}
+copyIcon.addEventListener("click", () => {
+  if (passBox.value != "" || passBox.value.length >= 1) {
+    navigator.clipboard.writeText(passBox.value);
+    copyIcon.innerHTML = "check";
+    copyIcon.title = "password Copied";
+    setTimeout(() => {
+      copyIcon.innerHTML = "content_copy";
+      copyIcon.title = "";
+    }, 3000);
+  }
+});
